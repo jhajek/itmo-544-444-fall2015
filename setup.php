dff8ef5c8567917dc06cee846087a827fafeb1c0
Jeremy Hajek <hajek@iit.edu>
2015-10-22 15:35:16 -0500

    Added RDS setup.php

---
+++ setup.php
@@ -0,0 +1,54 @@
+<?php
+// Start the session^M
+require 'vendor/autoload.php';^M
+$rds = new Aws\Rds\RdsClient([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
+
+$result = $rds->createDBInstance([
+    'AllocatedStorage' => 10,
+    #'AutoMinorVersionUpgrade' => true || false,
+    #'AvailabilityZone' => '<string>',
+    #'BackupRetentionPeriod' => <integer>,
+   # 'CharacterSetName' => '<string>',
+   # 'CopyTagsToSnapshot' => true || false,
+   # 'DBClusterIdentifier' => '<string>',
+    'DBInstanceClass' => 'db.t1.micro', // REQUIRED
+    'DBInstanceIdentifier' => 'mp1-jrh', // REQUIRED
+    #'DBName' => 'customerrecords',
+    #'DBParameterGroupName' => '<string>',
+    #'DBSecurityGroups' => ['<string>', ...],
+    #'DBSubnetGroupName' => '<string>',
+    'Engine' => 'MySQL', // REQUIRED
+    'EngineVersion' => '5.5.41',
+    #'Iops' => <integer>,
+    #'KmsKeyId' => '<string>',
+   # 'LicenseModel' => '<string>',
+  'MasterUserPassword' => 'letmein888',
+    'MasterUsername' => 'controller',
+    #'MultiAZ' => true || false,
+    #'OptionGroupName' => '<string>',
+    #'Port' => <integer>,
+    #'PreferredBackupWindow' => '<string>',
+    #'PreferredMaintenanceWindow' => '<string>',
+    'PubliclyAccessible' => true,
+    #'StorageEncrypted' => true || false,
+    #'StorageType' => '<string>',
+   # 'Tags' => [
+   #     [
+   #         'Key' => '<string>',
+   #         'Value' => '<string>',
+   #     ],
+        // ...
+   # ],
+    #'TdeCredentialArn' => '<string>',
+    #'TdeCredentialPassword' => '<string>',
+   # 'VpcSecurityGroupIds' => ['<string>', ...],
+]);
+
+print "Create RDS DB results: \n";
+print_r($rds);
+
+?>
+
